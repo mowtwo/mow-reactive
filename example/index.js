305d5ded273f832866f25b172a429e000461796e
mowtwo <1046099511@qq.com>
2022-04-14 16:37:16 +0800

    添加演示案例

--- example/index.js
+++ example/index.js
@@ -1,9 +1,19 @@
 import { effect, state } from "../lib/index.js";
 
+const log = console.log.bind(console, "%c[log]:", "color:green");
+
+// Vue的ref风格
 const a = state(0);
+// Solid风格
+const [b, setB] = state(0);
 
 effect(() => {
-  console.log(a.value);
+  log("a:" + a.value);
+  log("b:" + b());
 });
 
-a.value += 1;
+a.value = 2;
+
+setB(b() + 1);
+
+setB((b) => b + 1);
